1f42744a135d552d2c638666ad4ff1758e31675a
Tony Bowden <tony@tmtm.com>
2022-05-14 10:53:33 +0300

    move official comparison to sources/

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -15,5 +15,5 @@ class Comparison < EveryPoliticianScraper::NulllessComparison
   end
 end
 
-diff = Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+diff = Comparison.new('wikidata.csv', 'scraped.csv').diff
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
